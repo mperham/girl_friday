4490661a5678d4ee522f67cb9c98b858f2890d3f
Tony Arcieri <tony.arcieri@gmail.com>
2011-05-18 23:00:13 -0600

    Remove old actor implementation

--- lib/girl_friday.rb
+++ lib/girl_friday.rb
@@ -2,15 +2,6 @@ require 'thread'
 require 'weakref'
 require 'celluloid'
 
-begin
-  # Rubinius
-  require 'actor'
-  require 'girl_friday/monkey_patches'
-rescue LoadError
-  # Others
-  require 'girl_friday/actor'
-end
-
 require 'girl_friday/version'
 require 'girl_friday/work_queue'
 require 'girl_friday/error_handler'
