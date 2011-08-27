1b2a57d2d31933f3c88de2411f3c58dd4ad02bf4
Mike Perham <mperham@gmail.com>
2011-08-27 11:32:07 -0700

    Remove Rails environment hackery.  Explicitly use this in an initializer instead:
    GirlFriday::Queue.immediate! if Rails.env.development?

--- lib/girl_friday/work_queue.rb
+++ lib/girl_friday/work_queue.rb
@@ -37,13 +37,8 @@ module GirlFriday
       result
     end
 
-    if defined?(Rails) && Rails.env.development?
-      Rails.logger.debug "[girl_friday] Starting in single-threaded mode for Rails autoloading compatibility" if Rails.logger
-      alias_method :push_async, :push_immediately
-    else
-      def push_async(work, &block)
-        @supervisor << Work[work, block]
-      end
+    def push_async(work, &block)
+      @supervisor << Work[work, block]
     end
     alias_method :push, :push_async
     alias_method :<<, :push_async
--- test/test_girl_friday_immediately.rb
+++ test/test_girl_friday_immediately.rb
@@ -46,34 +46,4 @@ class TestGirlFridayImmediately < MiniTest::Unit::TestCase
     assert_equal 3, queue.push(:start => 2)
     assert_equal 3, queue << {:start => 2}
   end
-
-  def test_should_process_immediately_when_rails_dev
-    # remove WorkQueue definitions first, since #push_async is defined
-    # as the class definition is executed, and Rails constant is not defined yet
-    [:Queue, :WorkQueue].each {|c| GirlFriday.send(:remove_const, c)}
-
-    # now top-level Rails constant will exist
-    Object.send(:include, RailsEnvironment)
-
-    # re-load class now that Rails constant is defined
-    $".delete_if {|f| f =~ %r{work_queue.rb}}
-    require 'girl_friday/work_queue'
-
-    # verify that we process immediately
-    queue = GirlFriday::WorkQueue.new('now') do |msg|
-      msg[:start] + 1
-    end
-    assert_equal 4, queue.push(:start => 3)
-    assert_equal 4, queue << {:start => 3}
-  end
-end
-
-module RailsEnvironment
-  class Rails
-    def self.method_missing(m, *args, &block)
-      # swallow every call and return self so that WorkQueue
-      # thinks Rails dev environment is loaded
-      self
-    end
-  end
 end
