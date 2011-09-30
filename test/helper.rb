9046e61bde0553478a3c90f0f97b590c0bc30221
Mike Perham <mperham@gmail.com>
2011-09-30 07:33:19 -0700

    Have tests' at_exit sleep to minimize shutdown race conditions

--- test/helper.rb
+++ test/helper.rb
@@ -2,12 +2,15 @@ $testing = true
 puts RUBY_DESCRIPTION
 
 at_exit do
+  # queue.shutdown is async so sleep a little to minimize
+  # race conditions between us and other threads
+  # that haven't yet been GC'd.
+  sleep 0.1
   if Thread.list.size > 1
     Thread.list.each do |thread|
       next if thread.status == 'run'
       puts "WARNING: lingering threads found.  All threads should be shutdown and garbage collected."
-      p [thread, thread[:label], thread.object_id]
-#      puts thread.backtrace.join("\n")
+      p [thread, thread[:label]]
     end
   end
 end
